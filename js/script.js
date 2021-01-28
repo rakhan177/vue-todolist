3ca242a9f44bf7db6286b75fb161041568795257
Matteo <metboolean@gmail.com>
2021-01-28 17:47:00 +0100

    finito

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ var app = new Vue({
   el: '#root',
   data: {
     // url immagine
-    logo: 'img/logo.png',
+    logo: {
+      src:'img/logo.png',
+      alt: 'logo',
+    },
+    // classe i
+    iClass: {
+
+    },
     // placeholder input
     placeholder: 'Aggiungi Elemento',
     // array elementi
@@ -11,14 +18,26 @@ var app = new Vue({
       'Elemento 2',
       'Elemento 3',
     ],
+    // elemento inserito
     nuovoElemento: '',
   },
   methods: {
+    // se l' utente ha scritto qualcosa, lo aggiungo all' array
     aggiungiElemento(){
+      if(this.nuovoElemento !== ''){
       this.lista.push(this.nuovoElemento);
+      // pulisco input
       this.nuovoElemento = '';
-    }
+     }
+    },
+    /*funzione elimina con argomento index che filtra gli elementi di un array
+    e restituisce in lista solo gli elementi che hanno indice diverso dall'
+    elemento cliccato*/
+    eliminaElemento(index){
+      this.lista = this.lista.filter((elem, index1) => {
+      return index !== index1;
+      })
+    },
   }
-
-
 })
+Vue.config.devtools = true;
