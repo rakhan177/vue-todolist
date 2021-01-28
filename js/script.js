5a6ffc17d0bdd4505df3167daa8543b4a28baed1
Matteo <metboolean@gmail.com>
2021-01-28 16:09:30 +0100

    evrwc

--- js/script.js
+++ js/script.js
@@ -11,9 +11,13 @@ var app = new Vue({
       'Elemento 2',
       'Elemento 3',
     ],
+    nuovoElemento: '',
   },
   methods: {
-    
+    aggiungiElemento(){
+      this.lista.push(this.nuovoElemento);
+      this.nuovoElemento = '';
+    }
   }
 
 
