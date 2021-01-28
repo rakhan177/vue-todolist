4fd19b28494196b52e3b7d8ea284b716a3aa7acb
Matteo <metboolean@gmail.com>
2021-01-28 15:48:30 +0100

    dgfs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 var app = new Vue({
   el: '#root',
   data: {
-    nome: 'Matteo',
+    // url immagine
+    logo: 'img/logo.png',
+    // placeholder input
+    placeholder: 'Aggiungi Elemento',
+    // array elementi
+    lista: [
+      'Elemento 1',
+      'Elemento 2',
+      'Elemento 3',
+    ],
+  },
+  methods: {
+    
   }
 
 
